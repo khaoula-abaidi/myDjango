4972b87a121d58ab6d62af6d89f6c58f157e01d4
khaoula-abaidi <abaidik@gmail.com>
2019-12-10 23:49:21 +0100

    view questions with html views

--- mycourses/polls/views.py
+++ mycourses/polls/views.py
@@ -5,13 +5,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Question
+
+from django.template import loader, RequestContext
 # Create your views here.
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
+    #output = ", ".join(q.question_text for q in latest_questions)
 
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context =  {
+        'latest_questions':latest_questions
+    }
+    return HttpResponse(template.render(context))
+    #return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse('This is the detail view question  : %s' % question_id)
