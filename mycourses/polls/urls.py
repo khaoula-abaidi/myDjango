97e21267566912bc5f28af74b6393f4e925f8f80
khaoula-abaidi <abaidik@gmail.com>
2019-12-11 13:28:39 +0100

    include && extends templates 2

--- mycourses/polls/urls.py
+++ mycourses/polls/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from .import views
 
+app_name = 'polls'
 urlpatterns = [
     #127.0.0.1:8000/polls
     url(r'^$', views.index, name= "index"),
 
     # 127.0.0.1:8000/1
     url(r'^(?P<question_id>[0-9]+/$)',views.detail, name= "detail"),
-
     # 127.0.0.1:8000/1/results
     url(r'^(?P<question_id>[0-9]+/results$)', views.results, name="results"),
 
