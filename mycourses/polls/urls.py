3826685825e6bb272a81671f0d628d61cde662f1
khaoula-abaidi <abaidik@gmail.com>
2019-12-11 21:10:01 +0100

    url with path corrections

--- mycourses/polls/urls.py
+++ mycourses/polls/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 from .import views
 
 app_name = 'polls'
@@ -7,7 +8,8 @@ urlpatterns = [
     url(r'^$', views.index, name= "index"),
 
     # 127.0.0.1:8000/1
-    url(r'^(?P<question_id>[0-9]+/$)',views.detail, name= "detail"),
+    #url(r'^(?P<question_id>[0-9]+/$)',views.detail, name= "detail"),
+    path('<question_id>/', views.detail, name = "detail"),
     # 127.0.0.1:8000/1/results
     url(r'^(?P<question_id>[0-9]+/results$)', views.results, name="results"),
 
